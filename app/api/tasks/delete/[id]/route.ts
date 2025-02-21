d6e523cb63ed8d7147f54f8cd1a569e01b97d88b
oroshi80 <oroshi@gmail.com>
2025-02-21 11:55:49 +0000

    fix(id): number or string.

--- app/api/tasks/delete/[id]/route.ts
+++ app/api/tasks/delete/[id]/route.ts
@@ -7,7 +7,7 @@ interface QueryResult {
     affectedRows: number;
 }
 
-export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
+export async function DELETE(req: NextRequest, { params }: { params: { id: number | string } }) {
     // Wait for params to be resolved
 
     const { id } = await params; // Await params before destructuring
